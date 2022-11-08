bc6b05fe9887a2078fb207d37d35a0907d59e530
unknown <teun.rensing27@gmail.com>
2022-11-08 11:28:20 +0100

    update code

--- Shift_reg/build/esp-idf/main/cmake_install.cmake
+++ Shift_reg/build/esp-idf/main/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: D:/git/APPS/Shift_Reg/main
+# Install script for directory: D:/git/APPS/shift_reg/main
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Shift_Reg")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/shift_reg")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
--- Shift_reg/build/esp-idf/mbedtls/mbedtls/cmake_install.cmake
+++ Shift_reg/build/esp-idf/mbedtls/mbedtls/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Shift_Reg")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/shift_reg")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -39,9 +39,9 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("D:/git/APPS/Shift_Reg/build/esp-idf/mbedtls/mbedtls/include/cmake_install.cmake")
-  include("D:/git/APPS/Shift_Reg/build/esp-idf/mbedtls/mbedtls/3rdparty/cmake_install.cmake")
-  include("D:/git/APPS/Shift_Reg/build/esp-idf/mbedtls/mbedtls/library/cmake_install.cmake")
+  include("D:/git/APPS/shift_reg/build/esp-idf/mbedtls/mbedtls/include/cmake_install.cmake")
+  include("D:/git/APPS/shift_reg/build/esp-idf/mbedtls/mbedtls/3rdparty/cmake_install.cmake")
+  include("D:/git/APPS/shift_reg/build/esp-idf/mbedtls/mbedtls/library/cmake_install.cmake")
 
 endif()
 
