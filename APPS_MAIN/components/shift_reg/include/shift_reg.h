57e80977077f670239309ec9f0db8b333cad3366
unknown <teun.rensing27@gmail.com>
2022-12-13 10:09:16 +0100

    Update code

--- APPS_MAIN/components/shift_reg/include/shift_reg.h
+++ APPS_MAIN/components/shift_reg/include/shift_reg.h
@@ -1,19 +1,53 @@
 #ifndef SHIFT_REG_H
 #define SHIFT_REG_H
 
-#define OE GPIO_NUM_25
-#define RCLK GPIO_NUM_26
-#define SER GPIO_NUM_32
-#define SRSCLK GPIO_NUM_27
+#include <stdio.h>
+#include "driver/gpio.h"
 
 #define SR_LENGTH 8
 #define HIGH 1
 #define LOW 0
+#define SHIFT_MASK(x) (1ULL << x)
+#define PBM_SR_CONFIG(w, x, y, z) (SHIFT_MASK(w) | SHIFT_MASK(x) | SHIFT_MASK(y) | SHIFT_MASK(z))
 
-static const char *TAG = "example";
+typedef struct
+{
 
-void config_shift_register(void);
-void shift_register_update_memory(void);
-void shift_register_update_output(void);
-void shift_register_insert(int position, uint8_t value);
+    char get[SR_LENGTH];
+
+} shift_reg_state;
+
+typedef struct
+{
+    gpio_num_t OE,
+        RCLK,
+        SER,
+        SRCLK;
+
+} shift_register_pins;
+
+typedef struct
+{
+    shift_register_pins sr_pins;
+    gpio_config_t io_config;
+    shift_reg_state sr_state;
+
+} shift_register;
+
+// set the pins of the shift register
+void set_pins_shift_register(shift_register *config, gpio_num_t OE, gpio_num_t RCLK, gpio_num_t SER, gpio_num_t SRCLK);
+// config a shift register
+void config_shift_register(shift_register config);
+// shift register memory update
+void shift_register_update_memory(shift_register sr_config);
+// shift register set what is in the memory of the shift register to the output
+void shift_register_update_output(shift_register sr_config);
+// set a pin high or low based on the position, value and which shift shift register
+void shift_register_set(int position, char value, shift_register *config);
+// set all low
+void shift_register_setup(shift_register *config);
+// Set a shift register in series
+void set_pins_serie_shift_register(shift_register config1, shift_register *config2);
+// combining 2 shift registers in series set function
+void shift_register_com_update_memory(shift_register config1, shift_register config2);
 #endif
--- APPS_MAIN/components/shift_reg/src/shift_reg.c
+++ APPS_MAIN/components/shift_reg/src/shift_reg.c
@@ -14,54 +14,103 @@
 #include "sdkconfig.h"
 #include "shift_reg.h"
 
-char shift_reg_state[] = {"00000000"};
-
-void config_shift_register(void)
+void set_pins_shift_register(shift_register *config, gpio_num_t sr_OE, gpio_num_t sr_RCLK, gpio_num_t sr_SER, gpio_num_t sr_SRCLK)
 {
-    gpio_config_t io_config = {};
-    io_config.intr_type = GPIO_INTR_DISABLE;
-    io_config.mode = GPIO_MODE_OUTPUT;
-    io_config.pin_bit_mask = ((1ULL << OE) | (1ULL << RCLK) | (1ULL << SER) | (1ULL << SRSCLK));
-    io_config.pull_down_en = 0;
-    io_config.pull_up_en = 0;
-    gpio_set_level(OE, 0);
-    gpio_set_level(SER, 0);
-    gpio_set_level(RCLK, 0);
-    gpio_set_level(SRSCLK, 0);
-    gpio_config(&io_config);
+    //Set the pins for the shift register
+    config->sr_pins.OE = sr_OE;     // output enable
+    config->sr_pins.RCLK = sr_RCLK; 
+    config->sr_pins.SER = sr_SER;
+    config->sr_pins.SRCLK = sr_SRCLK;
 }
 
-void shift_register_update_memory(void)
+void config_shift_register(shift_register config)
+{
+    config.io_config.intr_type = GPIO_INTR_DISABLE;
+    config.io_config.mode = GPIO_MODE_OUTPUT;
+    config.io_config.pin_bit_mask = PBM_SR_CONFIG(config.sr_pins.OE, config.sr_pins.SER, config.sr_pins.RCLK, config.sr_pins.SRCLK);
+    config.io_config.pull_down_en = LOW;
+    config.io_config.pull_up_en = LOW;
+    gpio_config(&config.io_config);
+    gpio_set_level(config.sr_pins.OE, LOW);
+    gpio_set_level(config.sr_pins.SER, LOW);
+    gpio_set_level(config.sr_pins.RCLK, LOW);
+    gpio_set_level(config.sr_pins.SRCLK, LOW);
+}
+
+void shift_register_update_memory(shift_register sr_config)
 {
     /*Update shift register memory*/
     for (int i = SR_LENGTH; i >= 0; i--)
     {
-        gpio_set_level(SER, ((shift_reg_state[i] == '1') ? HIGH : LOW));
-        gpio_set_level(SRSCLK, HIGH);
-        gpio_set_level(SRSCLK, LOW);
+        gpio_set_level(sr_config.sr_pins.SER, ((sr_config.sr_state.get[i] == '1') ? HIGH : LOW));
+        gpio_set_level(sr_config.sr_pins.SRCLK, HIGH);
+        gpio_set_level(sr_config.sr_pins.SRCLK, LOW);
     }
 }
 
-void shift_register_update_output(void)
+void shift_register_update_output(shift_register sr_config)
 {
     /*Set array from memory to output*/
-    gpio_set_level(RCLK, HIGH);
-    gpio_set_level(RCLK, LOW);
+    gpio_set_level(sr_config.sr_pins.RCLK, HIGH);
+    gpio_set_level(sr_config.sr_pins.RCLK, LOW);
 }
 
-void shift_register_insert(int position, uint8_t value)
+void shift_register_set(int position, char value, shift_register *config)
 {
     /*Array for extended gpio*/
 
     if (position >= 0 && position <= SR_LENGTH)
     {
-        if (value == '1')
+        if (value == '1' || value == '0')
         {
-            shift_reg_state[position] = '1';
+            if (value == '1')
+            {
+                config->sr_state.get[position] = '1';
+            }
+            else
+            {
+                config->sr_state.get[position] = '0';
+            }
         }
         else
         {
-            shift_reg_state[position] = '0';
+            printf("not a correct value, %c", value);
         }
+    }
+    else
+    {
+        printf("not a correct position %d", position);
+    }
+}
+void shift_register_setup(shift_register *config)
+{
+    for (int i = 0; i < 8; i++)
+    {
+        shift_register_set(i, '0', config);
+    }
+}
+void set_pins_serie_shift_register(shift_register config1, shift_register *config2)
+{
+    config2->sr_pins.OE = config1.sr_pins.OE;
+    config2->sr_pins.SER = config1.sr_pins.SER;
+    config2->sr_pins.SRCLK = config1.sr_pins.SRCLK;
+    config2->sr_pins.RCLK = config1.sr_pins.RCLK;
+}
+void shift_register_com_update_memory(shift_register config1, shift_register config2)
+{
+    /*Update shift register memory*/
+
+    for (int i = 15; i >= 0; i--)
+    {
+        if (i < 8)
+        {
+            gpio_set_level(config1.sr_pins.SER, ((config1.sr_state.get[i] == '1') ? HIGH : LOW));
+        }
+        else
+        {
+            gpio_set_level(config1.sr_pins.SER, ((config2.sr_state.get[i-SR_LENGTH] == '1') ? HIGH : LOW));
+        }
+        gpio_set_level(config1.sr_pins.SRCLK, HIGH);
+        gpio_set_level(config1.sr_pins.SRCLK, LOW);
     }
 }
