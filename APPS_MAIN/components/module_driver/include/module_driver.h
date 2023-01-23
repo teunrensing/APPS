5ee89acebcd2c90082009294ca865084a1a659ae
Victor Hogeweij <vo.hogeweij@student.han.nl>
2023-01-23 13:06:51 +0100

    -add module driver recognition and pinning

--- APPS_MAIN/components/module_driver/include/module_driver.h
+++ APPS_MAIN/components/module_driver/include/module_driver.h
@@ -1,5 +1,67 @@
 #ifndef MODULE_DRIVER_H
 #define MODULE_DRIVER_H
 
+typedef enum{
+    pin_type_physical, pin_type_shift_register, pin_type_io_expander, pin_type_adc_chan
+}pin_type_t;
+
+typedef struct{
+    pin_type_t type;
+    uint8_t location;
+}pin_t;
+
+typedef enum {
+    LED_ADDRESSABLE, TENS, MOTOR
+}module_types_t;
+
+typedef struct {
+     uint8_t test;
+}led_drv_t;
+
+typedef struct {
+    uint8_t test;
+}tens_drv_t;
+
+typedef struct {
+    uint8_t test;
+}motor_drv_t;
+
+typedef union {
+    led_drv_t led_driver;
+    tens_drv_t tens_driver;
+    motor_drv_t motor_driver;
+}driver_t;
+
+
+typedef struct {
+    uint8_t I2C_SCL;
+    uint8_t I2C_SDA;
+    pin_t PWM1;
+    pin_t PWM2; 
+    pin_t TTL1;
+    pin_t TTL2;
+    pin_t TTL3;
+    pin_t STATUS;
+    uint8_t ID_ADC_NUM;
+    uint8_t ID_ADC_CH;
+}module_slot_pins_t;
+
+
+typedef struct{
+    uint8_t module_slot_num;
+    module_slot_pins_t pins;
+    module_types_t type;
+    driver_t module_driver_1;
+    driver_t module_driver_2;
+}module_slot_drv_t;
+
+module_slot_drv_t Module_slot_1;
+
+#ifdef TWO_MODULE_SLOTS
+module_slot_drv_t Module_slot_2;
+#endif
+
+esp_err_t init_module_slot_pins(module_slot_drv_t* slot);
+esp_err_t init_module_drivers(module_slot_drv_t* slot);
 
 #endif
--- APPS_MAIN/components/module_driver/src/module_driver.c
+++ APPS_MAIN/components/module_driver/src/module_driver.c
@@ -6,3 +6,90 @@
 #include "sdkconfig.h"
 #include "module_driver.h"
 
+esp_err_t init_module_slot_pins(module_slot_drv_t* slot){
+    module_slot_pins_t *module_pins = &(slot->pins);
+
+    #ifdef MODULE_SLOTS_SHARE_I2C_BUS
+        module_pins->I2C_SCL = CONFIG_MODULE_SLOTS_I2C_BUS_MAPPING_SCL;
+        module_pins->I2C_SDA = CONFIG_MODULE_SLOTS_I2C_BUS_MAPPING_SDA;
+    #endif
+
+    if(slot->module_slot_num == 1){
+        module_pins->PWM1.location = CONFIG_MODULE_SLOT_1_PWM_CH0;
+        module_pins->PWM2.location = CONFIG_MODULE_SLOT_1_PWM_CH1;
+
+        #ifdef CONFIG_MODULE_1_TTL1_IS_PHYSICAL_PIN
+        module_pins->TTL1.location = CONFIG_MODULE_SLOT_1_TTL1_PHYSICAL;
+        module_pins->TTL1.type = pin_type_physical;
+        #else
+        module_pins->TTL1.location = CONFIG_MODULE_SLOT_1_TTL1_SR;
+        module_pins->TTL1.type = pin_type_shift_register;
+        #endif
+
+        #ifdef CONFIG_MODULE_1_TTL2_IS_PHYSICAL_PIN
+        module_pins->TTL2.location = CONFIG_MODULE_SLOT_1_TTL2_PHYSICAL;
+        module_pins->TTL2.type = pin_type_physical;
+        #else
+        module_pins->TTL2.location = CONFIG_MODULE_SLOT_1_TTL2_SR;
+        module_pins->TTL2.type = pin_type_shift_register;
+        #endif
+
+        #ifdef CONFIG_MODULE_1_TTL3_IS_PHYSICAL_PIN
+        module_pins->TTL3.location = CONFIG_MODULE_SLOT_1_TTL3_PHYSICAL;
+        module_pins->TTL3.type = pin_type_physical;
+        #else
+        module_pins->TTL3.location = CONFIG_MODULE_SLOT_1_TTL3_IO_EXP;
+        module_pins->TTL3.type = pin_type_io_expander;
+        #endif
+
+        #ifdef CONFIG_MODULE_1_STATUS_IS_PHYSICAL_PIN
+        module_pins->STATUS.location = CONFIG_MODULE_SLOT_1_STATUS_PHYSICAL;
+        module_pins->STATUS.type = pin_type_physical;
+        #else
+        module_pins->STATUS.location = CONFIG_MODULE_SLOT_1_STATUS_SR;
+        module_pins->STATUS.type = pin_type_shift_register;
+        #endif
+        
+        module_pins->ID_ADC_CH = CONFIG_MODULE_SLOT_1_ID_ADC_CH;
+        module_pins->ID_ADC_NUM = CONFIG_MODULE_SLOT_1_ID_ADC_NUM;
+    }
+    else{
+        module_pins->PWM1.location = CONFIG_MODULE_SLOT_2_PWM_CH0;
+        module_pins->PWM2.location = CONFIG_MODULE_SLOT_2_PWM_CH1;
+
+        #ifdef CONFIG_MODULE_2_TTL1_IS_PHYSICAL_PIN
+        module_pins->TTL1.location = CONFIG_MODULE_SLOT_2_TTL1_PHYSICAL;
+        module_pins->TTL1.type = pin_type_physical;
+        #else
+        module_pins->TTL1.location = CONFIG_MODULE_SLOT_2_TTL1_SR;
+        module_pins->TTL1.type = pin_type_shift_register;
+        #endif
+
+        #ifdef CONFIG_MODULE_2_TTL2_IS_PHYSICAL_PIN
+        module_pins->TTL2.location = CONFIG_MODULE_SLOT_2_TTL2_PHYSICAL;
+        module_pins->TTL2.type = pin_type_physical;
+        #else
+        module_pins->TTL2.location = CONFIG_MODULE_SLOT_2_TTL2_SR;
+        module_pins->TTL2.type = pin_type_shift_register;
+        #endif
+
+        #ifdef CONFIG_MODULE_2_TTL3_IS_PHYSICAL_PIN
+        module_pins->TTL3.location = CONFIG_MODULE_SLOT_2_TTL3_PHYSICAL;
+        module_pins->TTL3.type = pin_type_physical;
+        #else
+        module_pins->TTL3.location = CONFIG_MODULE_SLOT_2_TTL3_IO_EXP;
+        module_pins->TTL3.type = pin_type_io_expander;
+        #endif
+
+        #ifdef CONFIG_MODULE_2_STATUS_IS_PHYSICAL_PIN
+        module_pins->STATUS.location = CONFIG_MODULE_SLOT_2_STATUS_PHYSICAL;
+        module_pins->STATUS.type = pin_type_physical;
+        #else
+        module_pins->STATUS.location = CONFIG_MODULE_SLOT_2_STATUS_SR;
+        module_pins->STATUS.type = pin_type_shift_register;
+        #endif
+        module_pins->ID_ADC_CH = CONFIG_MODULE_SLOT_2_ID_ADC_CH;
+        module_pins->ID_ADC_NUM = CONFIG_MODULE_SLOT_2_ID_ADC_NUM;
+    }
+    return ESP_OK;
+}
