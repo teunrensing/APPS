be8beb961f7e9a3016e9245f11fd6018187e904c
Victor Hogeweij <vo.hogeweij@student.han.nl>
2023-01-28 12:23:13 +0100

    -enh Documented functions in the module_driver.h file

--- APPS_MAIN/components/module_driver/include/module_driver.h
+++ APPS_MAIN/components/module_driver/include/module_driver.h
@@ -44,11 +44,74 @@ typedef struct{
     module_parameters_t parameters;
 }module_slot_drv_t;
 
+/**
+ * @brief Map the module slot pin types and locations defined in the kconfig (esp-menuconfig)
+ *        to the module_slot.
+ *
+ * @param slot The slot to map the pins to
+ *
+ * @return
+ *     - ESP_OK  Success!
+ *     - ESP_ERR Error!
+ */
 esp_err_t init_module_slot_pins(module_slot_drv_t* slot);
+
+/**
+ * @brief Init the drivers in the slot (module_driver_1 and module_driver_2) according to the module type
+ *
+ * @param slot The slot which contains the drivers to initialize
+ *
+ * @return
+ *     - ESP_OK  Success!
+ *     - ESP_ERR Error!
+ */
 esp_err_t init_module_drivers(module_slot_drv_t* slot);
+
+/**
+ * @brief Turn the module on. Watch out! Will turn on all the drivers inside the slot.
+ * This might be undesired!
+ *
+ * @param slot The slot to be turned on
+ *
+ * @return
+ *     - ESP_OK  Success!
+ *     - ESP_ERR Error!
+ */
 esp_err_t turn_module_on(module_slot_drv_t* slot);
+
+/**
+ * @brief Turn the module off. Watch out! Will turn off all the drivers inside the slot.
+ * This might be undesired!
+ *
+ * @param slot The slot to be turned off
+ *
+ * @return
+ *     - ESP_OK  Success!
+ *     - ESP_ERR Error!
+ */
 esp_err_t turn_module_off(module_slot_drv_t* slot);
+
+/**
+ * @brief For some modules its needed to refresh them once in a while. 
+ * This can be due to monitoring reasons or other reasons.
+ *
+ * @param slot The slot to be refreshed
+ *
+ * @return
+ *     - ESP_OK  Success!
+ *     - ESP_ERR Error!
+ */
 esp_err_t refresh_module(module_slot_drv_t* slot);
+
+/**
+ * @brief This function can be used to set the feedback parameters for a module.
+ *
+ * @param slot The slot to get new parameters.
+ *
+ * @return
+ *     - ESP_OK  Success!
+ *     - ESP_ERR Error!
+ */
 esp_err_t set_module_parameters(module_parameters_t* parameters);
 
 #endif
